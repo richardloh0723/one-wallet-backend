e2cb4127c5545de3f2c5799a83cf97cef7165108
Loh Jing Hoong <jinghoong@cyber-village.net>
2023-05-28 09:27:15 +0800

    add main method

---
+++ src/main/java/com/paynethackathon/dashboard/main/Main.java
@@ -0,0 +1,11 @@
+package com.paynethackathon.dashboard.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext("com.paynethackathon");
+		
+	}
+}
